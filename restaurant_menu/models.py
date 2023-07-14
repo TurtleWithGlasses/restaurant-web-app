23d980c86018e453dae564ecf9416f1d239740ce
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-14 17:27:48 +0300

    Create admin, fixed bug

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -24,5 +24,5 @@ class Item(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
-    def __init__(self):
+    def __str__(self):
         return self.meal
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -6,7 +6,7 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    def get_context_data(self):
         context = {"meals" : ["Pizza", "Pasta"],
                    "ingredients" : ["things"]}
         return context
